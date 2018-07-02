6844512fb2adb543c02992018cf39e79dd9b1807
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-07-02 19:53:24 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,3 +7,19 @@ function paragraphSelector() {
 function lastImageSelector() {
   $('img:last')
 }
+
+function ninjaBabySelector() {
+  $('img#baby-ninja')
+}
+
+function divSelector() {
+  $('div.pics')
+}
+
+function firstListItem() {
+  $('ul#pic-list li:first-child')
+}
+
+function () {
+  $('')
+}
